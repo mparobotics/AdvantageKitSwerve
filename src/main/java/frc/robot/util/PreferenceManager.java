a917842fed719588604f03f2cd8c0ea600045b14
jmmaloney3 <jmmaloney3@gmail.com>
2026-09-24 21:56:49 -0500

    checked in versions formatted by spotless - only formatting changes and removal of one unused import

--- src/main/java/frc/robot/util/PreferenceManager.java
+++ src/main/java/frc/robot/util/PreferenceManager.java
@@ -1,48 +1,47 @@
 package frc.robot.util;
 
+import edu.wpi.first.math.controller.PIDController;
+import frc.robot.util.TunableVariable.TunableDouble;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkBase;
-import frc.robot.util.TunableVariable.TunableDouble;
-
-import edu.wpi.first.math.controller.PIDController;
-
-
 public class PreferenceManager {
 
-    public class TunablePID{
-        private static final Map<String, TunableDouble[]> PIDMap = new HashMap<>();
+  public class TunablePID {
+    private static final Map<String, TunableDouble[]> PIDMap = new HashMap<>();
 
-        public void ApplyTunableValues(String name, PIDController controller) {
-            TunableDouble[] values = PIDMap.computeIfAbsent(name, subsystemName -> new TunableDouble[]{
-                new TunableDouble(subsystemName + "kP", controller.getP()),
-                new TunableDouble(subsystemName + "kI", controller.getI()),
-                new TunableDouble(subsystemName + "kD", controller.getD())
-            });
+    public void ApplyTunableValues(String name, PIDController controller) {
+      TunableDouble[] values =
+          PIDMap.computeIfAbsent(
+              name,
+              subsystemName ->
+                  new TunableDouble[] {
+                    new TunableDouble(subsystemName + "kP", controller.getP()),
+                    new TunableDouble(subsystemName + "kI", controller.getI()),
+                    new TunableDouble(subsystemName + "kD", controller.getD())
+                  });
 
-        if(values[0].hasChanged() || values[1].hasChanged() || values[2].hasChanged()) {
-            controller.setPID(values[0].get(),values[1].get(), values[2].get());
-            }
-         }
-
- /*       public static void autoTune(PIDController controller, SparkBase motor, RelativeEncoder encoder, double defaultkP, double setpoint, double tolerance) {
-            double startPos = encoder.getPosition();
-        
-            TunableDouble[] PIDArray = new TunableDouble[]{
-                new TunableDouble("Auto kP", defaultkP), 
-                new TunableDouble("Auto kI", 0.0),
-                new TunableDouble("Auto KD", 0.0)};
-
-            TunableDouble[] lastPIDValues = PIDArray;
-
-            controller.setTolerance(tolerance);
-            double output = controller.calculate(encoder.getPosition(), setpoint);
-            double percentOutput = output/12; //12 volts is battery volts
-            motor.set(percentOutput);
-
-            
-        } */
+      if (values[0].hasChanged() || values[1].hasChanged() || values[2].hasChanged()) {
+        controller.setPID(values[0].get(), values[1].get(), values[2].get());
+      }
     }
+
+    /*       public static void autoTune(PIDController controller, SparkBase motor, RelativeEncoder encoder, double defaultkP, double setpoint, double tolerance) {
+        double startPos = encoder.getPosition();
+
+        TunableDouble[] PIDArray = new TunableDouble[]{
+            new TunableDouble("Auto kP", defaultkP),
+            new TunableDouble("Auto kI", 0.0),
+            new TunableDouble("Auto KD", 0.0)};
+
+        TunableDouble[] lastPIDValues = PIDArray;
+
+        controller.setTolerance(tolerance);
+        double output = controller.calculate(encoder.getPosition(), setpoint);
+        double percentOutput = output/12; //12 volts is battery volts
+        motor.set(percentOutput);
+
+
+    } */
+  }
 }
--- src/main/java/frc/robot/util/TunableVariable.java
+++ src/main/java/frc/robot/util/TunableVariable.java
@@ -1,113 +1,110 @@
 package frc.robot.util;
 
 import edu.wpi.first.wpilibj.Preferences;
-public class TunableVariable{
 
-    public static class TunableDouble {
-        private String key;
-        private double lastValue;
+public class TunableVariable {
 
-        public TunableDouble(String key, double defaultValue) {
-            this.key = key;
-            Preferences.initDouble(key, defaultValue);
-            lastValue = Preferences.getDouble(key, defaultValue);
-        }
+  public static class TunableDouble {
+    private String key;
+    private double lastValue;
 
-        public double get() {
-            return Preferences.getDouble(key, lastValue);
-        }
-
-        public void set(double number) {
-            Preferences.setDouble(key, number);
-        }
-
-        public boolean hasChanged() {
-            double currentValue = get();
-            if(lastValue != currentValue) {
-                lastValue = currentValue;
-                return true;
-            }
-            else {return false;}
-        }
+    public TunableDouble(String key, double defaultValue) {
+      this.key = key;
+      Preferences.initDouble(key, defaultValue);
+      lastValue = Preferences.getDouble(key, defaultValue);
     }
 
-    public static class TunableInt {
-        private String key;
-        private int lastValue;
-
-        public TunableInt(String key, int defaultValue) {
-            this.key = key;
-            Preferences.initInt(key, defaultValue);
-            lastValue = Preferences.getInt(key, defaultValue);
-        }
-
-        public int get() {
-            return Preferences.getInt(key, lastValue);
-        }
-
-        public boolean hasChanged() {
-            int currentValue = get();
-            if(lastValue != currentValue) {
-                lastValue = currentValue;
-                return true;
-            }
-            else {return false;}
-        }
-
+    public double get() {
+      return Preferences.getDouble(key, lastValue);
     }
 
-    public static class TunableBoolean {
+    public void set(double number) {
+      Preferences.setDouble(key, number);
+    }
+
+    public boolean hasChanged() {
+      double currentValue = get();
+      if (lastValue != currentValue) {
+        lastValue = currentValue;
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  public static class TunableInt {
+    private String key;
+    private int lastValue;
+
+    public TunableInt(String key, int defaultValue) {
+      this.key = key;
+      Preferences.initInt(key, defaultValue);
+      lastValue = Preferences.getInt(key, defaultValue);
+    }
+
+    public int get() {
+      return Preferences.getInt(key, lastValue);
+    }
+
+    public boolean hasChanged() {
+      int currentValue = get();
+      if (lastValue != currentValue) {
+        lastValue = currentValue;
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  public static class TunableBoolean {
     private String key;
     private boolean lastValue;
 
     public TunableBoolean(String key, Boolean defaultValue) {
-        this.key = key;
-        Preferences.initBoolean(key, defaultValue);
-        lastValue = Preferences.getBoolean(key, defaultValue);
+      this.key = key;
+      Preferences.initBoolean(key, defaultValue);
+      lastValue = Preferences.getBoolean(key, defaultValue);
     }
 
     public boolean get() {
-        return Preferences.getBoolean(key, lastValue);
+      return Preferences.getBoolean(key, lastValue);
     }
 
     public boolean hasChanged() {
-        boolean currentValue = get();
-        if(lastValue != currentValue) {
-            lastValue = currentValue;
-            return true;
-        }
-        else {return false;}
+      boolean currentValue = get();
+      if (lastValue != currentValue) {
+        lastValue = currentValue;
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  public static class TunableFloat {
+    private String key;
+    private float lastValue;
+
+    public TunableFloat(String key, float defaultValue) {
+      this.key = key;
+      Preferences.initFloat(key, defaultValue);
+      lastValue = Preferences.getFloat(key, defaultValue);
     }
 
-}
-
-    public static class TunableFloat {
-        private String key;
-        private float lastValue;
-
-        public TunableFloat(String key, float defaultValue) {
-            this.key = key;
-            Preferences.initFloat(key, defaultValue);
-            lastValue = Preferences.getFloat(key, defaultValue);
-        }
-
-        public float get() {
-            return Preferences.getFloat(key, lastValue);
-        }
-
-        public boolean hasChanged() {
-            float currentValue = get();
-            if(lastValue != currentValue) {
-                lastValue = currentValue;
-                return true;
-            }
-            else {return false;}
-        }
-
+    public float get() {
+      return Preferences.getFloat(key, lastValue);
     }
 
-
+    public boolean hasChanged() {
+      float currentValue = get();
+      if (lastValue != currentValue) {
+        lastValue = currentValue;
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 }
-
-
-
